3a0bf32cf584206e16779246fa9ac8c19c0aff07
Mayur Sarman Odedra <56889920+MayurSarmanOdedra@users.noreply.github.com>
2020-07-11 23:24:40 +1200

    Update app.rb

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ class Cars < ActiveRecord::Base
   self.table_name = 'salesforcecarsdb.car__c'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/cars" do
+  @cars = Cars.all
+  erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
